78d2de065e37e68a662ba6f89c7f6a5adc646949
BlackSmokingOfMoon <104624954+tr1ma1d@users.noreply.github.com>
2023-06-14 21:34:29 +0300

    Добавьте файлы проекта.

---
+++ codewars trainng my skiills)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars_trainng_my_skiills_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = "is2 Thi1s T4est 3a ds6 lo5ck";
+            
+            Console.WriteLine(string.Join(" ", words.Split(' ').OrderBy(x => x.ToList().Find(c => char.IsDigit(c)))));
+
+            Console.WriteLine();
+            
+  
+            Console.ReadKey();
+        }
+    }
+}
